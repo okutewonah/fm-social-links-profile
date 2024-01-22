a09000784e1f14855c3b10250dc742ce50baf141
Okute <okute@Okutes-MacBook-Air.local>
2024-01-22 18:38:30 +0200

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,13 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      green: "hsl(75, 94%, 57%)",
+      white: "hsl(0, 0%, 100%)",
+      grey: "hsl(0, 0%, 20%)",
+      "dark-grey": "hsl(0, 0%, 12%)",
+      "off-black": "hsl(0, 0%, 8%)",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
